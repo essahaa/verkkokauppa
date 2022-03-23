b3f3c0323dd9aa199d75407f394496b9eb75f245
AtteTaajo <32447990+AtteTaajo@users.noreply.github.com>
2022-03-23 11:05:06 +0200

    backendjutui(keskeneräsiä)

---
+++ verkkokauppa/BackEnd/inc/functions.php
@@ -0,0 +1,14 @@
+<?php
+function openDb(): object {
+    $ini= parse_ini_file("../config.ini", true);
+
+    $host = $ini["host"];
+    $database = $ini["database"];
+    $user = $ini["user"];
+    $password = $ini["password"];
+    
+
+/*Hullua paskaa*/
+
+}
+}

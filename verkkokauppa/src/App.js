840453517ca2b391f422ebf3a2a90640e85bee1a
Atte Taajoranta <n9taat00@students.oamk.fi>
2022-03-24 11:03:07 +0200

    kuvakatos

--- verkkokauppa/src/App.js
+++ verkkokauppa/src/App.js
@@ -146,7 +146,7 @@ function App() {
                     </div>
                     <div className='col'>
                         <div className="card">
-                            <img src={require('pictures/red_bull.jpg')} className="card-img-top" alt="..." />
+                            <img className="card-img-top" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">Red Bull</h5>
                                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>

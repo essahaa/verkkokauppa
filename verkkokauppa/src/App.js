493d71ae38cc38f65fd93ed71920bc88b937348f
Dolez777 <73564898+Dolez777@users.noreply.github.com>
2022-04-03 18:53:06 +0300

    Route asennus, säätöä

--- verkkokauppa/src/App.js
+++ verkkokauppa/src/App.js
@@ -1,23 +1,36 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.scss';
 import Header from './components/header';
+import Home from './pages/Home';
+import Products from './pages/Products';
 function App() {
 	return (
 		<div className="App">
 			<Header></Header>
 			<div className="container">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/Products" element={<Products />} />
+				</Routes>
 				<div className="row">
 					<div className="nav col-3 col-xs-3 col-sm-3 col-md-2 col-lg-1">
 						<ul className="list">
 							<li className="listItem">
-								<a href="#" className="navlink">
+								{/* 	<a href="#" className="navlink">
 									Etusivu
-								</a>
+								</a> */}
+								<Link to="/verkkokauppa/src/pages/Home.js">
+									Etusivu
+								</Link>
 							</li>
 							<li className="listItem">
-								<a href="#" className="navlink">
+								{/* 	<a href="#" className="navlink">
 									Etusivu
-								</a>
+								</a> */}
+								<Link to="/verkkokauppa/src/pages/Products.js">
+									Tuotteet
+								</Link>
 							</li>
 							<li className="listItem">
 								<a href="#" className="navlink">
---
+++ verkkokauppa/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../components/header';
+
+export default function home() {
+	return (
+		<div>
+			<Header></Header>
+			<div id="allo">Home POKEMON</div>;
+		</div>
+	);
+}

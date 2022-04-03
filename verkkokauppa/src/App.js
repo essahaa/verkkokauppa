13fb7202739a1e2cd172e3ac6ff0b707961b5cd1
Atte Taajoranta <n9taat00@students.oamk.fi>
2022-04-03 13:33:11 +0300

    classit -> classNameksi

--- verkkokauppa/src/App.js
+++ verkkokauppa/src/App.js
@@ -20,7 +20,7 @@ function App() {
 				</div>
 			</header>
 
-			<body>
+			<div className='bod'>
 				<div className="row">
 					<div className="nav col-3 col-xs-3 col-sm-3 col-md-2 col-lg-1">
 						<ul className="list">
@@ -54,60 +54,60 @@ function App() {
 
 					<div className="col-9 col-xs-9 col-sm-9 col-md-10 col-lg-11 row">
 						<div className="kortti col-xs-12 col-sm-6 col-md-4 col-lg-3">
-							<div class="card">
+							<div className="card">
 								<img
 									src={require('./images/battery.png')}
 									className="card-img-top"
 									alt="..."
 								/>
-								<div class="card-body">
-									<h5 class="card-title">Card title</h5>
-									<a href="#" class="btn btn-primary">
+								<div className="card-body">
+									<h5 className="card-title">Card title</h5>
+									<a href="#" className="btn btn-primary">
 										Go somewhere
 									</a>
 								</div>
 							</div>
 						</div>
 						<div className="kortti col-xs-12 col-sm-6 col-md-4 col-lg-3">
-							<div class="card">
+							<div className="card">
 								<img
 									src={require('./images/battery.png')}
 									className="card-img-top"
 									alt="..."
 								/>
-								<div class="card-body">
-									<h5 class="card-title">Card title</h5>
-									<a href="#" class="btn btn-primary">
+								<div className="card-body">
+									<h5 className="card-title">Card title</h5>
+									<a href="#" className="btn btn-primary">
 										Go somewhere
 									</a>
 								</div>
 							</div>
 						</div>
 						<div className="kortti col-xs-12 col-sm-6 col-md-4 col-lg-3">
-							<div class="card">
+							<div className="card">
 								<img
 									src={require('./images/battery.png')}
 									className="card-img-top"
 									alt="..."
 								/>
-								<div class="card-body">
-									<h5 class="card-title">Card title</h5>
-									<a href="#" class="btn btn-primary">
+								<div className="card-body">
+									<h5 className="card-title">Card title</h5>
+									<a href="#" className="btn btn-primary">
 										Go somewhere
 									</a>
 								</div>
 							</div>
 						</div>
 						<div className="kortti col-xs-12 col-sm-6 col-md-4 col-lg-3">
-							<div class="card">
+							<div className="card">
 								<img
 									src={require('./images/battery.png')}
 									className="card-img-top"
 									alt="..."
 								/>
-								<div class="card-body">
-									<h5 class="card-title">Card title</h5>
-									<a href="#" class="btn btn-primary">
+								<div className="card-body">
+									<h5 className="card-title">Card title</h5>
+									<a href="#" className="btn btn-primary">
 										Go somewhere
 									</a>
 								</div>
@@ -115,7 +115,7 @@ function App() {
 						</div>
 					</div>
 				</div>
-			</body>
+			</div>
 		</div>
 	);
 }

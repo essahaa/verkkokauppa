9c63219e9908d7d4446aa662084f5b6239d340e3
Atte Taajoranta <n9taat00@students.oamk.fi>
2022-04-04 16:08:17 +0300

    backia

--- verkkokauppa/src/components/navbar/index.jsx
+++ verkkokauppa/src/components/navbar/index.jsx
@@ -1,23 +1,39 @@
-import React from 'react';
+
 import { Link } from 'react-router-dom';
 import './navbar.scss';
+import axios from 'axios';
+import React,{useState, useEffect} from 'react';
 
-function Navbar() {
+export default function Navbar({url}) {
+	const [gategories,setGategories] = useState([]);
+
+	useEffect(() => {
+		console.log(url);
+		axios.get(url + "products/getcategories.php")
+		.then((response) => {
+			const json = response.data;
+			setGategories(json);
+			console.log(json)
+		}).catch (error => {
+			alert(error.response === undefined ? error : error.response.data.error);
+		})
+	}, [])
+}
+
+function nav() {
 	return (
 		<div className="row">
 			<div className="nav col-3 col-xs-3 col-sm-3 col-md-2 col-lg-1">
 				<ul className="list">
-					<li className="listItem">
-						{/* 	<a href="#" className="navlink">
-									Etusivu
-								</a> */}
-						<Link
-							className="links"
-							to="/verkkokauppa/src/pages/Home.js"
-						>
-							Etusivu
-						</Link>
-					</li>
+					{categories.map(category => (
+						<li className="listItem">
+							<Link
+								className="links"
+								to={'products/' + category.id}>
+									{gategory.name}
+							</Link>
+						</li>
+					))}
 					<li className="listItem">
 						{/* 	<a href="#" className="navlink">
 									Etusivu
@@ -30,9 +46,12 @@ function Navbar() {
 						</Link>
 					</li>
 					<li className="listItem">
-						{/* <a href="#" className="navlink">
-							Etusivu
-						</a> */}
+						<Link
+							className="links"
+							to={'products/' + category.id}>{gategory.name}
+						
+							
+						</Link>
 					</li>
 					<li className="listItem">
 						{/* <a href="#" className="navlink">
@@ -49,4 +68,4 @@ function Navbar() {
 		</div>
 	);
 }
-export default Navbar;
+

6a6ce1b70fe8e4688578367a277dd640df070846
Atte Taajoranta <n9taat00@students.oamk.fi>
2022-04-06 14:54:36 +0300

    productsBackki

--- verkkokauppa/src/components/body/index.jsx
+++ verkkokauppa/src/components/body/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import Products from '../../pages/Products';
 import './body.scss';
 export default function body() {
+
+	
+
 	return (
 		<div className="items-container">
 			<div className="kortti col-xs-12 col-sm-6 col-md-4 col-lg-3">
@@ -11,10 +15,13 @@ export default function body() {
 						alt="..."
 					/>
 					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
+						{products.map(product => (
+						<h5 className="card-title" key={product.id}>{product.name}</h5>
+						))}
 						<a href="#" className="btn btn-primary">
 							Go somewhere
 						</a>
+						
 					</div>
 				</div>
 			</div>

685856415bb851c2763ac12f770424c706368bb7
Atte Taajoranta <n9taat00@students.oamk.fi>
2022-04-17 11:40:18 +0300

    kuva koodia

--- verkkokauppa/src/pages/Products.js
+++ verkkokauppa/src/pages/Products.js
@@ -28,21 +28,24 @@ export default function Product({url}) {
 return (
 
 <div className="kortti col-xs-12 col-sm-6 col-md-4 col-lg-3">
+	{products.map(product => (
 	<div className="card">
+		
 		<img
-			src={'/images/battery.png'}
+			src={url + "images/" + product.image}
 			className="card-img-top"
 			alt="..."
 		/>
 		<div className="card-body">
-			{products.map(product => (
+			
 			<h5 className="card-title" key={product.id}>{product.name}</h5>
-			))}
+		
 			<a href="#" className="btn btn-primary">
 				Go somewhere
 			</a>
 		</div>
 	</div>
+		))}
 </div>
 
 )}

ddfeafb155bff7c75c048bb491dbb1114e4fc363
Dolez777 <73564898+Dolez777@users.noreply.github.com>
2022-04-19 07:54:58 +0300

    -nodemodules

--- verkkokauppa/src/App.js
+++ verkkokauppa/src/App.js
@@ -11,7 +11,7 @@ function App() {
 		<div className="App">
 			<Header></Header>
 			<Routes>
-				<Route path="*" element={<Home />} />
+				<Route path="/" element={<Home />} />
 				<Route path="/Products" element={<Products />} />
 				<Route path="/ProductDetail" element={<ProductDetail/>}/>
 			</Routes>
